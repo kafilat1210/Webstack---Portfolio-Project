ef1dafe6e46744255755e26421d13fa297d0fb6c
kafilat1210 <cafylhearty@gmail.com>
2024-12-30 11:00:33 +0100

    Started working on the App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,9 +2,34 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
+    const url = "https://";
+    
   return (
     <div className="App">
-	  
+	<div className="container">
+	    <div className="top">
+		<div className="location">
+		    <p>Lagos</p>
+		</div>
+		<div className="temp">
+		    <h1>80 degree</h1>
+		</div>
+		<div className="description">
+		    <p>Clouds</p>
+		</div>
+	    </div>
+	    <div className="bottom">
+		<div className="feels">
+		    <p>80 degree</p>
+		</div>
+		<div className="humidity">
+		    <p>20%</p>
+		</div>
+		<div className="wind">
+		    <p>12 KPH</p>
+		</div>
+	    </div>
+	</div>
     </div>
   );
 }
