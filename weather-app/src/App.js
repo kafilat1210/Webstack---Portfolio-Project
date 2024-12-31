dfa6bb55f63fb3697135a0b30c3547889908ed1a
kafilat1210 <cafylhearty@gmail.com>
2024-12-31 18:14:26 +0100

    updated the App.js for the api call

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,8 +2,19 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
-    //const url = "https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=2e4b21a881b0390e190e93a28c239a15";
-    
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2e4b21a881b0390e190e93a28c239a15`;
+
+    const searchData = (event) => {
+	if (event.key === 'Enter') {
+	    axios.get(url).then((response) => {
+		setData(response.data)
+		console.log(response.data)
+	    })
+	}
+    }
   return (
     <div className="app">
 	<div className="container">
